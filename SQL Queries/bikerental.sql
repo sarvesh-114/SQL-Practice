1e71c090218885281f34c5612621fec7efe03c12
Sarvesh Jathar <sarveshjathar448@gmail.com>
2025-11-12 17:35:51 +0530

    Update bikerental.sql

--- SQL Queries/bikerental.sql
+++ SQL Queries/bikerental.sql
@@ -12,25 +12,43 @@ revenue .
  Sort the results chronologically. Display the year total after all the month 
 totals for the corresponding year. Show the all-time total as the last row.*/
 
-select 
-extract(year from start_timestamp) as year,
-extract(month from start_timestamp) as month,
-sum(total_paid) as total_amt
+-- Answer 1
+select extract(year from start_timestamp) as year
+, extract(month from start_timestamp) as month
+, sum(total_paid) as revenue
 from rental
-group by year, month
+group by extract(year from start_timestamp), extract(month from start_timestamp)
 union all
-select 
-extract(year from start_timestamp) as year,
-null as month,
-sum(total_paid) as total_amt
+select extract(year from start_timestamp) as year
+, null as month, sum(total_paid) as revenue
 from rental
-group by year
+group by extract(year from start_timestamp)
 union all
-select 
-null as year,
-null as month,
-sum(total_paid) as total_amt
+select null as year, null as month, sum(total_paid) as revenue
 from rental
-group by year, month
+order by year, month;
+
+--Answer 2
+select extract(year from start_timestamp) as year
+, extract(month from start_timestamp) as month
+, sum(total_paid) as revenue
+from rental
+group by grouping sets ((year, month), (year), ())
+order by year, month
+
+--Answer 3
+select extract(year from start_timestamp) as year
+, extract(month from start_timestamp) as month
+, sum(total_paid) as revenue
+from rental
+group by rollup (year, month)
+order by year, month
+
+
+
+
+
+
+
 
 
