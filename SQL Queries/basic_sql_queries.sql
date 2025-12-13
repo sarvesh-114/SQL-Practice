c43eac9729c49784974439c64f1ed1bf5b596a1f
Sarvesh Jathar <sarveshjathar448@gmail.com>
2025-12-13 19:35:48 +0530

    Add multiple SQL queries for employee and student data

--- SQL Queries/basic_sql_queries.sql
+++ SQL Queries/basic_sql_queries.sql
@@ -58,6 +58,70 @@ insert into duplicate values
 (4, 'anushka', 'sharma', 20000, 'sharmaa@gmail.com', 1, to_date('01-mar-2021','dd-mon-yyyy')),
 (5, 'biplab', 'parida', 30000, 'bparida@yahoo.com', 1, to_date('21-dec-2015','dd-mon-yyyy'));
 
+-- 1. SQL Query to update DateOfJoining to 15-jul-2012 for empid =1
+update employee
+set dateofjoining = '2012-07-15'
+where empid = 1
+
+-- 2. SQL Query to select all student name where age is greater than 22
+select * from STUDENT where AGE > 22;
+
+-- 3. SQL Query to Find all employee with Salary between 40000 and 80000?
+select * from employee
+where salary between 40000 and 80000;
+
+-- 4. SQL Query to display full name?
+select firstname || ' ' || lastname as fullname
+from employee
+
+-- 5. SQL Query to find name of employee beginning with S?
+select * from employee
+where firstname like 's%';
+
+-- 6. Write a query to fetch details of employees whose firstname ends with an alphabet ‘A’ and contains exactly five alphabets ?
+select * from employee
+where firstname like '____a';
+
+-- 7. Write an SQL query to fetch the employee FIRST names and replace the A with ‘@’:
+select replace(firstname, 'a', '@') as new_name from employee
+
+-- 8. Write an SQL query to update the employee names by removing leading and trailing spaces :
+update employee
+set firstname = trim(firstname)
+
+-- 9. Write an SQL query to fetch all the Employees details from Employee table who joined in the Year 2020:
+select * from employee
+where extract(year from dateofjoining) = 2020
+
+-- 10. Write an SQL query to fetch only odd rows / Even rows from the table :
+select * from employee
+where mod(empid, 2) = 0
+
+select * from employee
+where mod(empid, 2) = 1
+
+--11. Write an SQL query to create a new table with data and structure copied from another table:
+create table dummy_employee as (select * from employee)
+select * from dummy_employee
+
+-- 12. Write an SQL query to fetch top 3 HIGHEST salaries :
+select * from employee
+order by salary desc
+limit 3
+
+-- 13. Write a query to fetch the department-wise count of employees sorted by department’s count in ascending order:
+select dept, count(*) as no_of_emp
+from employee
+group by dept
+order by no_of_emp asc
+
+-- 14.Write a query to retrieve Department wise Maximum salary:
+select dept, salary, rnk from
+(select *,
+rank() over(partition by dept order by salary desc) as rnk
+from employee) as r
+where r.rnk = 1
+
 select * from employee;
 select * from duplicate;
 select * from computer;
