883939d85817de1a9efdbc21926ffd16dc78f3d2
Sarvesh Jathar <sarveshjathar448@gmail.com>
2026-01-17 13:08:22 +0530

    Add SQL queries for course, payments, and logs management
    Added SQL queries for creating and managing course, payments, lift_logs, and weather tables. Included queries for handling duplicates, cash payment checks, lift weight limits, and temperature comparisons.

--- SQL Queries/sql_practice2.sql
+++ SQL Queries/sql_practice2.sql
@@ -206,6 +206,125 @@ order by count(*) desc
 limit 1;
 
 
+create table course (
+id int,
+skill varchar(100),
+description varchar(255)
+);
+insert into course (id, skill, description) values
+(1, 'SQL Basics', 'Introduction to SQL queries'),
+(2, 'Data Modeling', 'Fundamentals of data design'),
+(3, 'Python', 'Learn Python for data'),
+(4, 'SQL Basics', 'Introduction to SQL queries'),
+(5, 'Data Modeling', 'Fundamentals of data design'),
+(6, 'Power BI', 'Data visualization with Power BI'),
+(7, 'Python', 'Learn Python for data');
+
+-- You have a table ‘course’ with potential duplicate rows. Write a query to delete all
+-- duplicate entries while keeping one copy.
+with cte as
+	(select *,
+	rank() over(partition by skill order by id) as rnk
+	from course)
+-- select id from cte where rnk>1
+
+delete from course
+where id in(select id from cte where rnk>1)
+
+select * from course;
+
+
+create table payments (
+order_id int,
+customer_id int,
+payment_type varchar(20),
+amount decimal(10,2)
+);
+insert into payments (order_id, customer_id, payment_type, amount)
+values
+(101, 1, 'Cash', 450.00),
+(102, 1, 'UPI', 300.00),
+(103, 2, 'Card', 700.00),
+(104, 3, 'UPI', 250.00),
+(105, 3, 'Card', 500.00),
+(106, 4, 'Cash', 600.00),
+(107, 5, 'UPI', 120.00),
+(108, 6, 'Card', 900.00);
+
+-- Write a query to find the customer_id of customers who have never made cash
+-- payment.
+select distinct customer_id from payments
+where customer_id not in (
+select distinct customer_id from payments 
+where payment_type = 'Cash')
+
+select * from payments;
+
+
+create table lift_logs (
+log_id INT,
+emp_id INT,
+weight INT,
+entry_time TIMESTAMP
+);
+insert into lift_logs (log_id, emp_id, weight, entry_time) values
+(1, 201, 70, '2024-12-01 09:00:00'),
+(2, 202, 82, '2024-12-01 09:01:00'),
+(3, 203, 68, '2024-12-01 09:02:00'),
+(4, 204, 75, '2024-12-01 09:03:00'),
+(5, 205, 90, '2024-12-01 09:04:00'),
+(6, 206, 88, '2024-12-01 09:05:00'),
+(7, 207, 95, '2024-12-01 09:06:00'),
+(8, 208, 92, '2024-12-01 09:07:00'),
+(9, 209, 85, '2024-12-01 09:08:00'),
+(10, 210, 80, '2024-12-01 09:09:00'),
+(11, 211, 78, '2024-12-01 09:10:00');
+
+
+select * from lift_logs;
+-- Suppose a lift has a weight limit of 750 kg. Employees enter one by one. Write a
+-- query to find the first entry event where the lift starts ringing due to the weight
+-- limit being exceeded. Return log_id, emp_id, and entry_time.
+with cte as 
+	(select log_id, emp_id, entry_time, weight,
+	sum(weight) over ( order by  entry_time
+	rows between unbounded preceding and current row
+	) as cum_weight
+	from lift_logs)
+
+select log_id, emp_id, entry_time
+from cte where cum_weight > 750
+limit 1
+
+
+
+CREATE TABLE weather (
+Days DATE,
+Temperature INT
+);
+INSERT INTO weather (Days, Temperature) VALUES
+('2024-01-01', 20),
+('2024-01-02', 22),
+('2024-01-03', 21),
+('2024-01-05', 25),
+('2024-01-06', 25),
+('2024-01-07', 28),
+('2024-01-08', 24),
+('2024-01-09', 26);
+
+-- Write a query to find the days when the temperature was higher than the previous
+-- day.
+
+with cte as (
+select *,
+lag(days) over(order by days) as prev_day,
+lag(temperature) over(order by days) as prev_temp
+from weather)
+
+select * from cte
+where temperature > prev_temp
+and age(days, prev_day) = interval '1 DAY'
+
 
 
 
